ded8bf85d4624aad291a5cc35f15d1c6a7a61b12
Edotnd <edotnd@gmail.com>
2020-09-26 16:37:25 +0900

    tokenSplit

--- Lexer/Lexer.py
+++ Lexer/Lexer.py
@@ -1,1 +1,12 @@
 import re, keyword
+
+tokenSplit = [
+    ('KEYWORD', '|'.join(keyword.kwlist)),
+    ('ID', r'[a-zA-Z_]\w*'),
+    ('NUM', r'\d+|\d+\.\d*|\.\d+'),
+    ('LPAREN', r'\('),
+    ('RPAREN', r'\)'),
+    ('PLUS', r'\+'), ('MINUS', r'\-'), ('MUL', r'\*'),  ('DIV', r'\/'),
+    ('COLON', r':'),
+    ('WHITESPACE', r'\s+')
+]
